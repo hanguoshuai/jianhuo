20fab645a847a17e51eb71e7919898d4604313e9
hanguoshuai <a340693306@qq.com>
2017-05-10 11:41:12 +0800

    4

--- static/mock/mock.js
+++ static/mock/mock.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-let swiper = require('./swipper');
-let list = require('./list');
-module.exports = function(){
-    return {
-        'swiper':swiper,
-        'list':list
-    }
-}
-=======
+
+
 let indexdata = require('./indexData.json');
 module.exports=function(){
   return {
     'indexdata':indexdata
   }
 }
->>>>>>> 920e2a1c866ed1dcb6dfd0ac53bd74ea780fc71d
+
