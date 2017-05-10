923c6276dabd1b00a76972c14ca5ebbe2a0f39d5
hanguoshuai <a340693306@qq.com>
2017-05-10 12:18:29 +0800

    wangdong thesecond

--- static/mock/mock.js
+++ static/mock/mock.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 
+var center = require('./center.json')
+var center1 = require('./center1.json')
 
 let indexdata = require('./indexData.json');
 module.exports=function(){
   return {
-    'indexdata':indexdata
-  }
-}
-
-=======
-var center = require('./center.json')
-var center1 = require('./center1.json')
-
-module.exports = function () {
-  return {
-    'center': center,
+    'indexdata':indexdata,
+     'center': center,
     'center1': center1
   }
 }
->>>>>>> wangpro
+
+
