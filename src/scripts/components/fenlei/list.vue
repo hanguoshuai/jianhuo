0e4e365299dbe9e3f558270150d92147cdf4621d
hanguoshuai <a340693306@qq.com>
2017-05-11 15:10:48 +0800

    4

--- src/scripts/components/fenlei/list.vue
+++ src/scripts/components/fenlei/list.vue
@@ -126,21 +126,9 @@
 
 </style>
 <script>
-        import Vue from 'vue';
-        import { Navbar, TabItem } from 'mint-ui';
-
-        Vue.component(Navbar.name, Navbar);
-        Vue.component(TabItem.name, TabItem);
+       
     export default {
-        data:function(){
-            selected:'1'
-            
-        },
-        method:{
-
-        },
-        mouted:function(){
-            console.log(0)
-        }
+       
+         
     }
 </script>
