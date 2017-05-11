9a16d268ca72aa6e5d134708620566fe58d48ec9
hanguoshuai <a340693306@qq.com>
2017-05-11 14:24:19 +0800

    the third

--- dist/static/mock/mock.js
+++ dist/static/mock/mock.js
@@ -1,14 +1,13 @@
 
 var center = require('./center.json')
 var center1 = require('./center1.json')
-var list = require('./list.json')
+
 let indexdata = require('./indexData.json');
 module.exports=function(){
   return {
     'indexdata':indexdata,
      'center': center,
-    'center1': center1,
-    'list':list
+    'center1': center1
   }
 }
 
--- src/scripts/components/fenlei/list.vue
+++ src/scripts/components/fenlei/list.vue
@@ -1,136 +1,45 @@
 <template lang="html">
-   <div class="m-list">
-        <div class='m-list-div'>
-             <div class='m-list-header'>
-            <div class="m-list-header-top">
-                <img src="../../../../static/images/m-list-top-01.jpg" alt="">
-            </div>
-            <div class="m-list-header-content">
-                <ul>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                          <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       
-                </ul>
-            </div>
-            
-            
-        </div>
-         <div class='m-list-header'>
-            <div class="m-list-header-top">
-                <img src="../../../../static/images/m-list-top-01.jpg" alt="">
-            </div>
-            <div class="m-list-header-content">
-                <ul>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                          <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       
-                </ul>
-            </div>
-            
-            
-        </div>
-         <div class='m-list-header'>
-            <div class="m-list-header-top">
-                <img src="../../../../static/images/m-list-top-01.jpg" alt="">
-            </div>
-            <div class="m-list-header-content">
-                <ul>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                          <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       
-                </ul>
-            </div>
-            
-            
-        </div>
-         <div class='m-list-header'>
-            <div class="m-list-header-top">
-                <img src="../../../../static/images/m-list-top-01.jpg" alt="">
-            </div>
-            <div class="m-list-header-content">
-                <ul>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                          <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       <li>
-                           <div>面膜</div>
-                           <img src="../../../../static/images/m-list-content-01.jpg" alt="">
-                       </li>
-                       
-                </ul>
-            </div>
-            
-            
-        </div>
-        </div>
-        
-   </div>
-       
-        
-    
- 
    
+    <div>
+<mt-navbar v-model="selected">
+  <mt-tab-item id="1">选项一</mt-tab-item>
+  <mt-tab-item id="2">选项二</mt-tab-item>
+  <mt-tab-item id="3">选项三</mt-tab-item>
+</mt-navbar>
 
-   
+<!-- tab-container -->
+<mt-tab-container v-model="selected">
+  <mt-tab-container-item id="1">
+    <mt-cell v-for="n in 10" :title="'内容 ' + n" />
+  </mt-tab-container-item>
+  <mt-tab-container-item id="2">
+    <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+  </mt-tab-container-item>
+  <mt-tab-container-item id="3">
+    <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+  </mt-tab-container-item>
+</mt-tab-container>
+    </div>
 </template>
 <style>
 
 </style>
 <script>
+        import Vue from 'vue';
+        import { Navbar, TabItem } from 'mint-ui';
 
-export default{
-   
- 
-}
+        Vue.component(Navbar.name, Navbar);
+        Vue.component(TabItem.name, TabItem);
+    export default {
+        data:function(){
+            selected:'1'
+            
+        },
+        method:{
+
+        },
+        mouted:function(){
+            console.log(0)
+        }
+    }
 </script>
